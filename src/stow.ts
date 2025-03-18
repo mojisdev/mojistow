fcc61679350fccf0623376eb01f4005bdbd79f12
Lucas Nørgård <lucasnrgaard@gmail.com>
2025-03-18 05:48:35 +0100

    chore: try fix

--- src/stow.ts
+++ src/stow.ts
@@ -6,44 +6,41 @@ import { authMiddleware } from "./middlewares/auth";
 export const STOW_ROUTER = new Hono().basePath("/stow");
 
 STOW_ROUTER.post(
-  "/:version/:item",
+  "/:version",
   authMiddleware,
   async (c) => {
-    const { version, item } = c.req.param();
+    const { version } = c.req.param();
     const body = await c.req.parseBody();
 
-    const file = Array.isArray(body.file) ? body.file : [body.file];
+    const file = Array.isArray(body.file) ? body.file[0] : body.file;
 
-    if (file.length === 0) {
+    if (file == null) {
       throw new HTTPException(400, {
         message: "No file uploaded",
       });
     }
 
-    for (const f of file) {
-      if (typeof f === "string") {
-        continue;
-      }
-
-      const tar = await parseTarGzip(await f.arrayBuffer(), {
-        filter(file) {
-        // if file is pax header, return false
-          if (Number.isNaN(file.type)) return false;
-          if (file.name.includes("PaxHeader")) return false;
-          // some files is prefixed with ._, ignore them
-          if (file.name.includes("._")) return false;
-          return true;
-        },
+    if (typeof file === "string") {
+      throw new HTTPException(400, {
+        message: "invalid file uploaded",
       });
+    }
 
-      for (const entry of tar) {
+    const tar = await parseTarGzip(await file.arrayBuffer(), {
+      filter(file) {
+        // if file is pax header, return false
+        if (Number.isNaN(file.type)) return false;
+        if (file.name.includes("PaxHeader")) return false;
+        // some files is prefixed with ._, ignore them
+        if (file.name.includes("._")) return false;
+        return true;
+      },
+    });
+
+    for (const entry of tar) {
+      const res = await c.env.EMOJI_DATA.put(`${version}/${entry.name}`, entry.text);
       // eslint-disable-next-line no-console
-        console.info(entry.type, entry.name);
-
-        const res = await c.env.EMOJI_DATA.put(`${version}/${item}.json`, entry.text);
-        // eslint-disable-next-line no-console
-        console.info({ key: res?.key, version: res?.version, uploaded: res?.uploaded, size: res?.size });
-      }
+      console.info({ key: res?.key, version: res?.version, uploaded: res?.uploaded, size: res?.size });
     }
 
     return c.json({
